0710019553d4e12099863b6df71d9da5985cf241
InomERP <info@inomerp.in>
2026-04-28 11:06:46 +0530

    Add files via upload

---
+++ inom_helpdesk_basic/__manifest__.py
@@ -0,0 +1,26 @@
+{
+    'name': 'Inom Helpdesk Basic',
+    'version' : '18.0.0.0.0',
+    'category' : 'helpdesk',
+    'summary' : ' Helpdesk Basic',
+    'description' : 'Basic Odoo helpdesk module manages internal tickets, '
+                    'uses states, assignment, priority, validations, chatter, and workflow '
+                    'for training understanding development concepts. ',
+    'author' : 'InomERP',
+    'website' : 'https://www.inomerp.in',
+    'depends': ['web','mail','base'],
+    'data': [
+             'security/helpdesk_security.xml',
+             'security/ir.model.access.csv',
+             'data/helpdesk_sequence.xml',
+             'report/helpdesk_ticket_report.xml',
+             'views/helpdesk_ticket_views.xml',
+             'views/helpdesk_ticket_kanban.xml',
+             'views/helpdesk_ticket_search.xml',
+             'views/helpdesk_menu.xml',
+    ],
+    'images': ['static/description/banner.png'],
+    'installable' : True,
+    'auto_install' : False,
+    'application' : True,
+}
---
+++ inom_helpdesk_basic/models/helpdesk_ticket.py
@@ -0,0 +1,65 @@
+from odoo import api, fields, models, _
+from odoo.exceptions import UserError
+
+class HelpdeskTicket(models.Model):
+    _name = 'helpdesk.ticket'
+    _inherit = ['mail.thread','mail.activity.mixin']
+    _description = 'Helpdesk Ticket'
+
+    ticket_number = fields.Char(string="Ticket Number")
+    subject = fields.Char(string="Subject")
+    description = fields.Text(string="Description")
+    priority = fields.Selection([('low', 'Low'),
+                                 ('medium', 'Medium'),
+                                 ('high', 'High')], default="medium", string="Priority")
+    assigned_to = fields.Many2one('res.users', string="Assigned To")
+    state = fields.Selection([('new', 'New'),
+                              ('in_process', 'In process'),
+                              ('resolved', 'Resolved'),
+                              ('closed', 'Closed')], default="new", string="Ticket State",group_expand='_group_expand_states')
+
+    @api.model_create_multi
+    def create(self, vals_list):
+        for vals in vals_list:
+            if not vals.get('ticket_number'):
+                vals['ticket_number'] = self.env['ir.sequence'].next_by_code('helpdesk.ticket') or '/'
+        return super().create(vals_list)
+
+    def _group_expand_states(self, values, domain):
+        return ['new', 'in_process', 'resolved', 'closed']
+
+    def in_process_ticket(self):
+        for res in self:
+            res.state = "in_process"
+            res.message_post(body="Ticket moved to In Progress")
+
+    def resolved_ticket(self):
+        for res in self:
+            res.state = "resolved"
+            res.message_post(body="Ticket has been resolved")
+
+    def close_ticket(self):
+        for res in self:
+            res.state = "closed"
+            res.message_post(body="Ticket closed")
+
+    def action_print_ticket_report(self):
+        return self.env.ref('inom_helpdesk_basic.action_helpdesk_ticket_report').report_action(self)
+
+    def write(self, vals):
+        for rec in self:
+            # Closed tickets cannot be modified at all
+            if rec.state == 'closed':
+                raise UserError(_("You cannot modify a closed ticket"))
+
+            # Only manager can change state
+            if 'state' in vals:
+                if not self.env.user.has_group('inom_helpdesk_basic.group_helpdesk_manager'):
+                    raise UserError(_("Only Helpdesk Manager can change state"))
+
+            # Only manager can assign
+            if 'assigned_to' in vals:
+                if not self.env.user.has_group('inom_helpdesk_basic.group_helpdesk_manager'):
+                    raise UserError(_("Only Helpdesk Manager can assign tickets"))
+
+        return super().write(vals)
