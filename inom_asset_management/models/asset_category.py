d3be5323645079db5ca759d27985403f33f9dc31
InomERP <info@inomerp.in>
2026-04-30 14:51:41 +0530

    Add files via upload

---
+++ inom_asset_management/models/asset_category.py
@@ -0,0 +1,40 @@
+from odoo import models, fields
+
+class AssetCategory(models.Model):
+    _name = 'asset.category.custom'
+    _description = 'Asset Category'
+
+    name = fields.Char("Asset Name", required=True)
+
+    # Accounts
+    journal_id = fields.Many2one('account.journal', string="Journal")
+    asset_account_id = fields.Many2one('account.account', string="Asset Account")
+    depreciation_account_id = fields.Many2one('account.account', string="Depreciation Account")
+    expense_account_id = fields.Many2one('account.account', string="Expense Account")
+
+    # Periodicity
+    method_time = fields.Selection([
+        ('number', 'Based on Number'),
+        ('end', 'Ending Date')
+    ], string="Computation", default='number')
+
+    method_number = fields.Integer("Number of Depreciations")
+    method_progress_factor = fields.Float("Degressive Factor")
+    method_period = fields.Integer("Period Length")
+    method_end = fields.Date("Ending Date")
+
+    # Options
+    auto_confirm = fields.Boolean("Auto Confirm")
+    group_entries = fields.Boolean("Group Entries")
+
+    depreciation_type = fields.Selection([
+        ('manual', 'Manual')
+    ], string="Depreciation Type", default='manual')
+
+    # Depreciation
+    method = fields.Selection([
+        ('linear', 'Linear'),
+        ('degressive', 'Degressive')
+    ], string="Method", default='linear')
+
+    prorata = fields.Boolean("Prorata")
