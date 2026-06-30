570ae0c99fd03b7cef3d9ccda4c7523ac125a5d4
InomERP <info@inomerp.in>
2026-06-30 12:43:31 +0530

    Update __manifest__.py

--- inom_stock_adjust_control/__manifest__.py
+++ inom_stock_adjust_control/__manifest__.py
@@ -30,6 +30,7 @@ Key features
     'company': 'InomERP Pvt Ltd',
     'maintainer': 'InomERP Pvt Ltd',
     'website': 'https://inomerp.in',
+    "live_test_url": "https://youtu.be/AYC1xp-arK0&t=480s",
     'license': 'OPL-1',
     'depends': ['stock', 'mail'],
     'data': [
