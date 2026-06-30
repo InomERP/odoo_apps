e714201a978d12f771b2f0060bd991551529fc39
InomERP <info@inomerp.in>
2026-06-30 12:33:32 +0530

    Delete inom_purchase_mail_attachment directory

--- inom_purchase_mail_attachment/models/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import res_company
-from . import product_document
-from . import mail_template
-from . import res_partner
-from . import res_config_settings
-from . import purchase_order
