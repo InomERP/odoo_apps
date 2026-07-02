16b0b7b84035435ff99bb72015372228f37883a3
InomERP <info@inomerp.in>
2026-07-02 14:07:47 +0530

    Update __manifest__.py

--- inom_purchase_backorder/__manifest__.py
+++ inom_purchase_backorder/__manifest__.py
@@ -20,6 +20,7 @@ the list view indicator, the form view badge and the search filter.
     'author': 'Inomerp',
     'website': 'https://inomerp.in',
     'support': 'info@inomerp.in',
+    'live_test_url': 'https://www.youtube.com/watch?v=D_G_aw3V3Og',
     'license': 'OPL-1',
     'depends': [
         'purchase',
