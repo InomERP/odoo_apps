9b75b198ef595abfb0f15b60a9abb89a7f9704b1
InomERP <info@inomerp.in>
2026-07-21 12:26:57 +0530

    Delete inom_university_academics directory

--- inom_university_academics/wizards/__init__.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from . import univ_timetable_generator_wizard
-from . import univ_attendance_bulk_wizard
-from . import univ_attendance_import_wizard
-from . import univ_exam_hall_wizard
-from . import univ_exam_invigilator_wizard
-from . import univ_exam_seat_wizard
-from . import univ_result_generate_wizard
-from . import univ_reevaluation_wizard
