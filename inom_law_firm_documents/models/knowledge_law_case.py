f56cefc618e2b2f3bb0a554996161d58cab0bb13
InomERP <info@inomerp.in>
2026-08-14 13:23:45 +0530

    Delete inom_law_firm_documents directory

--- inom_law_firm_documents/models/knowledge_law_case.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import fields, models
-
-
-class LawCase(models.Model):
-    _inherit = "law.case"
-
-    cited_case_law_ids = fields.Many2many("law.case.law", string="Cited Case Law")
-    referenced_act_ids = fields.Many2many("law.act", string="Referenced Acts")
